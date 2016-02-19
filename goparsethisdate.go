52b360caed16428a5a16877481cca422479bb026
Zack Scholl <zack.scholl@gmail.com>
2016-02-19 12:01:21 -0500

    Updated with channel version

--- goparsethisdate.go
+++ goparsethisdate.go
@@ -1,9 +1,53 @@
-package goparsethisdate
+package main
 
 import (
 	"fmt"
 	"time"
 )
+
+func ParseDateChannel(value string) (time.Time, error) {
+	years := []string{"2006", "06", ""}
+	months := []string{"Jan", "Jan.", "January", "01", "1", ""}
+	days := []string{"Mon", "Mon.", "Monday", "2", "02", "2nd", "2st", "2rd", "2th", ""}
+	delimiters := []string{"/", " ", "", ", "}
+	timeNumsFirst := []string{"3:04pm", "15:04", "3:04:05pm", "15:04:05", "3pm", ""}
+	c := make(chan time.Time, 2*len(delimiters)*len(delimiters)*len(delimiters)*len(days)*len(months)*len(years)*len(timeNumsFirst))
+	for _, year := range years {
+		for _, month := range months {
+			for _, day := range days {
+				for _, delimiter1 := range delimiters {
+					for _, delimiter2 := range delimiters {
+						for _, delimiter3 := range delimiters {
+							for _, timeNumFirst := range timeNumsFirst {
+								layout := timeNumFirst + delimiter3 + month + delimiter1 + day + delimiter2 + year
+
+								go func(layout string, value string) {
+									t, err := time.Parse(layout, value)
+									//fmt.Println(layout)
+									if err == nil {
+										c <- t
+									}
+								}(layout, value)
+
+								layout = month + delimiter1 + day + delimiter2 + year + delimiter3 + timeNumFirst
+
+								go func(layout string, value string) {
+									t, err := time.Parse(layout, value)
+									//fmt.Println(layout)
+									if err == nil {
+										c <- t
+									}
+								}(layout, value)
+							}
+						}
+					}
+				}
+			}
+		}
+	}
+	x := <-c
+	return x, nil
+}
 
 func ParseDate(value string) (time.Time, error) {
 	years := []string{"2006", "06", ""}
@@ -39,4 +83,13 @@ func ParseDate(value string) (time.Time, error) {
 	}
 
 	return time.Now(), fmt.Errorf("Could not find date")
+}
+
+func main() {
+	// start := time.Now()
+	// ParseDateChannel("Feb. 2nd 2016")
+	// fmt.Println(time.Since(start))
+	start := time.Now()
+	ParseDate("Feb. 2nd 2016")
+	fmt.Println(time.Since(start))
 }
--- goparsethisdate_test.go
+++ goparsethisdate_test.go
@@ -1,47 +1,47 @@
-package goparsethisdate
+package main
 
 import (
 	"fmt"
 	"testing"
 )
 
+// func Example1() {
+// 	date, _ := ParseDate("02/17/2016 3:05pm")
+// 	fmt.Println(date)
+// 	// Output: 2016-02-17 15:05:00 +0000 UTC
+// }
+
+// func Example2() {
+// 	date, _ := ParseDate("02/17/2016 3pm")
+// 	fmt.Println(date)
+// 	// Output: 2016-02-17 15:00:00 +0000 UTC
+// }
+
+// func Example3() {
+// 	date, _ := ParseDate("3pm 02/17/2016")
+// 	fmt.Println(date)
+// 	// Output: 2016-02-17 15:00:00 +0000 UTC
+// }
+
+// func Example4() {
+// 	date, _ := ParseDate("3pm, 02/17/2016")
+// 	fmt.Println(date)
+// 	// Output: 2016-02-17 15:00:00 +0000 UTC
+// }
+
+// func Example5() {
+// 	date, _ := ParseDate("February 17th, 2016")
+// 	fmt.Println(date)
+// 	// Output: 2016-02-17 00:00:00 +0000 UTC
+// }
+
+// func Example6() {
+// 	date, _ := ParseDate("Feb 1st 2016")
+// 	fmt.Println(date)
+// 	// Output: 2016-02-01 00:00:00 +0000 UTC
+// }
+
 func Example1() {
-	date, _ := ParseDate("02/17/2016 3:05pm")
-	fmt.Println(date)
-	// Output: 2016-02-17 15:05:00 +0000 UTC
-}
-
-func Example2() {
-	date, _ := ParseDate("02/17/2016 3pm")
-	fmt.Println(date)
-	// Output: 2016-02-17 15:00:00 +0000 UTC
-}
-
-func Example3() {
-	date, _ := ParseDate("3pm 02/17/2016")
-	fmt.Println(date)
-	// Output: 2016-02-17 15:00:00 +0000 UTC
-}
-
-func Example4() {
-	date, _ := ParseDate("3pm, 02/17/2016")
-	fmt.Println(date)
-	// Output: 2016-02-17 15:00:00 +0000 UTC
-}
-
-func Example5() {
-	date, _ := ParseDate("February 17th, 2016")
-	fmt.Println(date)
-	// Output: 2016-02-17 00:00:00 +0000 UTC
-}
-
-func Example6() {
-	date, _ := ParseDate("Feb 1st 2016")
-	fmt.Println(date)
-	// Output: 2016-02-01 00:00:00 +0000 UTC
-}
-
-func Example7() {
 	date, _ := ParseDate("Feb. 2nd 2016")
 	fmt.Println(date)
 	// Output: 2016-02-02 00:00:00 +0000 UTC
@@ -50,5 +50,17 @@ func Example7() {
 func BenchmarkParseDate(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		ParseDate("02/17/2016 3:05pm")
+	}
+}
+
+func Example2() {
+	date, _ := ParseDateChannel("Feb. 2nd 2016")
+	fmt.Println(date)
+	// Output: 2016-02-02 00:00:00 +0000 UTC
+}
+
+func BenchmarkParseDateChannel(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		ParseDateChannel("02/17/2016 3:05pm")
 	}
 }
